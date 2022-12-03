f6aaf2681d621846470232a1c738b9e3276a68aa
Bordeaux2294 <ford.trishanna876@gmail.com>
2022-12-03 18:09:42 -0500

    Everything should work now

--- dashboard.js
+++ dashboard.js
@@ -5,6 +5,23 @@ window.addEventListener("load", function(){
     let sup_btn = document.querySelector(".s");
     let result = document.querySelector(".result");
 
+
+
+    let url = `dashboard.php?filter=All`;
+        console.log(url);
+
+        fetch(url)
+        .then(function (response) {
+            if (response.ok) {
+            return response.text();
+            } else {
+            throw new Error(response.statusText);
+            }
+        })
+        .then(function (data) {
+            result.innerHTML = data;
+        });
+
     all_btn.addEventListener("click", function(event){
         event.preventDefault();
 
