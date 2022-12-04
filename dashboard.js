00ef5f7a24e6438ca2f286fa47a25687f23658f4
Bordeaux2294 <ford.trishanna876@gmail.com>
2022-12-04 01:36:13 -0500

    need to update css

--- dashboard.js
+++ dashboard.js
@@ -3,11 +3,33 @@ window.addEventListener("load", function(){
     let all_btn = document.querySelector(".all");
     let sl_btn = document.querySelector(".sl");
     let sup_btn = document.querySelector(".s");
+    let atm_btn = document.querySelector(".atm");
     let result = document.querySelector(".result");
+    
+
+    let url = `dashboard.php?filter=All`;
+        console.log(url);
+        
+        fetch(url)
+        .then(function (response) {
+            if (response.ok) {
+            return response.text();
+            } else {
+            throw new Error(response.statusText);
+            }
+        })
+        .then(function (data) {
+            result.innerHTML = data;
+
+        });
+
+        
+        
+    
 
     all_btn.addEventListener("click", function(event){
         event.preventDefault();
-
+        
         let url = `dashboard.php?filter=All`;
         console.log(url);
 
@@ -21,6 +43,33 @@ window.addEventListener("load", function(){
         })
         .then(function (data) {
             result.innerHTML = data;
+            function dummy(){
+                let link = document.getElementsByClassName("link");
+                console.log(link);
+                for (let index = 0; index < link.length; index++) {
+                    link.item(index).addEventListener("click",function(event){
+                        event.target.preventDefault;
+                        console.log("i was clicked");
+                        let element = link.item(index);
+                        let url = `dashboard.php?id=${element.id}`;
+                    
+        
+                        fetch(url)
+                            .then(function (response) {
+                            if (response.ok) {
+                            return response.text();
+                            } else {
+                            throw new Error(response.statusText);
+                            }
+                        })
+                        .then(function (data) {
+                            result.innerHTML = data;
+                        });
+                    }
+                    
+                )}
+            }
+            setTimeout(dummy,100);
         });
     })
 
@@ -28,7 +77,6 @@ window.addEventListener("load", function(){
         event.preventDefault();
 
         let url = `dashboard.php?filter=SalesLead`;
-        console.log(url);
 
         fetch(url)
         .then(function (response) {
@@ -40,6 +88,33 @@ window.addEventListener("load", function(){
         })
         .then(function (data) {
             result.innerHTML = data;
+            function dummy(){
+                let link = document.getElementsByClassName("link");
+                console.log(link);
+                for (let index = 0; index < link.length; index++) {
+                    link.item(index).addEventListener("click",function(event){
+                        event.target.preventDefault;
+                        console.log("i was clicked");
+                        let element = link.item(index);
+                        let url = `dashboard.php?id=${element.id}`;
+                    
+        
+                        fetch(url)
+                            .then(function (response) {
+                            if (response.ok) {
+                            return response.text();
+                            } else {
+                            throw new Error(response.statusText);
+                            }
+                        })
+                        .then(function (data) {
+                            result.innerHTML = data;
+                        });
+                    }
+                    
+                )}
+            }
+            setTimeout(dummy,100);
         });
     })
 
@@ -47,7 +122,6 @@ window.addEventListener("load", function(){
         event.preventDefault();
 
         let url = `dashboard.php?filter=Support`;
-        console.log(url);
 
         fetch(url)
         .then(function (response) {
@@ -59,6 +133,153 @@ window.addEventListener("load", function(){
         })
         .then(function (data) {
             result.innerHTML = data;
+            function dummy(){
+                let link = document.getElementsByClassName("link");
+                console.log(link);
+                for (let index = 0; index < link.length; index++) {
+                    link.item(index).addEventListener("click",function(event){
+                        event.target.preventDefault;
+                        console.log("i was clicked");
+                        let element = link.item(index);
+                        let url = `dashboard.php?id=${element.id}`;
+                    
+        
+                        fetch(url)
+                            .then(function (response) {
+                            if (response.ok) {
+                            return response.text();
+                            } else {
+                            throw new Error(response.statusText);
+                            }
+                        })
+                        .then(function (data) {
+                            result.innerHTML = data;
+                        });
+                    }
+                    
+                )}
+            }
+            setTimeout(dummy,100);
         });
     })
+
+    atm_btn.addEventListener("click", function(event){
+        event.preventDefault();
+
+        let url = `dashboard.php?filter=atm`;
+
+        fetch(url)
+        .then(function (response) {
+            if (response.ok) {
+            return response.text();
+            } else {
+            throw new Error(response.statusText);
+            }
+        })
+        .then(function (data) {
+            result.innerHTML = data;
+            function dummy(){
+                let link = document.getElementsByClassName("link");
+                console.log(link);
+                for (let index = 0; index < link.length; index++) {
+                    link.item(index).addEventListener("click",function(event){
+                        event.target.preventDefault;
+                        console.log("i was clicked");
+                        let element = link.item(index);
+                        let url = `dashboard.php?id=${element.id}`;
+                    
+        
+                        fetch(url)
+                            .then(function (response) {
+                            if (response.ok) {
+                            return response.text();
+                            } else {
+                            throw new Error(response.statusText);
+                            }
+                        })
+                        .then(function (data) {
+                            result.innerHTML = data;
+                        });
+                    }
+                    
+                )}
+            }
+            setTimeout(dummy,100);
+        });
+    })
+
+    // link.forEach(element => {
+    //     element.addEventListener("click", function(event){
+    //         event.target.preventDefault;
+    //         let id = event.target.id;
+    //         let url = `dashboard.php?id=${id}`;
+    //         console.log(id);
+    
+    //         fetch(url)
+    //         .then(function (response) {
+    //             if (response.ok) {
+    //             return response.text();
+    //             } else {
+    //             throw new Error(response.statusText);
+    //             }
+    //         })
+    //         .then(function (data) {
+    //             result.innerHTML = data;
+    //         });
+    //     })
+    // });
+    
+
+
+    // forlink.forEach(element => {
+    //     element.addEventListener("click", function(event){
+    //         event.target.preventDefault;
+    //         let id = event.target.id;
+    //         let url = `dashboard.php?id=${id}`;
+    //         console.log(id);
+    
+    //         fetch(url)
+    //         .then(function (response) {
+    //             if (response.ok) {
+    //             return response.text();
+    //             } else {
+    //             throw new Error(response.statusText);
+    //             }
+    //         })
+    //         .then(function (data) {
+    //             result.innerHTML = data;
+    //         });
+    //     })
+    // });
+
+
+    function dummy(){
+        let link = document.getElementsByClassName("link");
+        console.log(link);
+        for (let index = 0; index < link.length; index++) {
+            link.item(index).addEventListener("click",function(event){
+                event.target.preventDefault;
+                console.log("i was clicked");
+                let element = link.item(index);
+                let url = `dashboard.php?id=${element.id}`;
+            
+
+                fetch(url)
+                    .then(function (response) {
+                    if (response.ok) {
+                    return response.text();
+                    } else {
+                    throw new Error(response.statusText);
+                    }
+                })
+                .then(function (data) {
+                    result.innerHTML = data;
+                });
+            }
+            
+        )}
+    }
+    setTimeout(dummy,100);
+        
+    
 })
--- dashboard.php
+++ dashboard.php
@@ -1,24 +1,67 @@
 <?php
 $host = 'localhost';
 $dbname = 'dolphin_crm';
+$password = '';
+$username = 'root';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4",'root');
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4",$username,$password);
 
-$filtered = $_GET["filter"];
 
-if($filtered == "All"){
-    echo all_contacts($conn);
+
+if(isset($_GET["id"])){
+    //echo id1($conn);
+    $id = $_GET["id"];
+    echo displayinfo($conn,$id);
 }
 
-if($filtered == "Support"){
-    echo support($conn);
+if(isset($_GET["filter"])){
+    $filtered = $_GET["filter"];
+    if($filtered == "All"){
+        echo all_contacts($conn,$filtered);
+    } else if($filtered == "Support"){
+        echo support($conn,$filtered);
+    }else if($filtered == "SalesLead"){
+        $filtered="Sales Lead";
+        echo slead($conn,$filtered);
+    }else if($filtered == "atm"){
+        echo atm($conn,$filtered);
+    }
 }
 
-if($filtered == "SalesLead"){
-    echo slead($conn);
+function displayinfo($conn,$id){
+    $stmt = $conn->query("SELECT * FROM Contacts where id='$id'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    foreach($results as $row){
+        $name = $row["title"]. ' ' . $row["firstname"]. ' '.$row["lastname"];
+        echo $name;
+        echo $row["created_by"];
+        echo $row["updated_at"];
+        echo $row["email"];
+        echo $row["company"];
+        echo $row["telephone"];
+        echo $row["assigned_to"];
+        echo $row["type"];
+        
+    }
+
+    $stmt = $conn->query("SELECT * FROM Notes where contact_id='$id'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach($results as $row){
+        echo $row["created_by"];
+        echo $row["comment"];
+        echo $row["created_at"];
+        
+    }
+    
+
+    
 }
 
-function all_contacts($conn){
+
+
+function all_contacts($conn,$filtered){
     $stmt = $conn->query("SELECT * FROM Contacts");
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
@@ -31,12 +74,14 @@ function all_contacts($conn){
     echo "</tr>";
 
     foreach($results as $row){
+        $assign=$row["id"];
         $name = $row["title"]. ' ' . $row["firstname"]. ' '.$row["lastname"];
         echo "<tr>";
         echo "<td>".$name."</td>";
         echo "<td>".$row["email"]."</td>";
         echo "<td>".$row["company"]."</td>";
         echo "<td>".$row["type"]."</td>";
+        echo "<td><span class='link' id='$assign'>view</span></td>";
         echo "</tr>";
     }
 
@@ -44,8 +89,8 @@ function all_contacts($conn){
 
 }
 
-function support($conn){
-    $stmt = $conn->query("SELECT * FROM Contacts where type='Support'");
+function support($conn,$filtered){
+    $stmt = $conn->query("SELECT * FROM Contacts where type='$filtered'");
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
     echo "<table>";
@@ -57,12 +102,14 @@ function support($conn){
     echo "</tr>";
 
     foreach($results as $row){
+        $assign=$row["id"];
         $name = $row["title"]. ' ' . $row["firstname"]. ' '.$row["lastname"];
         echo "<tr>";
         echo "<td>".$name."</td>";
         echo "<td>".$row["email"]."</td>";
         echo "<td>".$row["company"]."</td>";
         echo "<td>".$row["type"]."</td>";
+        echo "<td><span class='link' id='$assign'>view</span></td>";
         echo "</tr>";
     }
 
@@ -70,8 +117,8 @@ function support($conn){
 
 }
 
-function slead($conn){
-    $stmt = $conn->query("SELECT * FROM Contacts where type='Sales Lead'");
+function slead($conn,$filtered){
+    $stmt = $conn->query("SELECT * FROM Contacts where type='$filtered'");
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
     echo "<table>";
@@ -83,12 +130,14 @@ function slead($conn){
     echo "</tr>";
 
     foreach($results as $row){
+        $assign=$row["id"];
         $name = $row["title"]. ' ' . $row["firstname"]. ' '.$row["lastname"];
         echo "<tr>";
         echo "<td>".$name."</td>";
         echo "<td>".$row["email"]."</td>";
         echo "<td>".$row["company"]."</td>";
         echo "<td>".$row["type"]."</td>";
+        echo "<td><span class='link' id='$assign'>view</span></td>";
         echo "</tr>";
     }
 
@@ -96,6 +145,34 @@ function slead($conn){
 
 }
 
+
+function atm($conn,$filtered){
+    $stmt = $conn->query("SELECT * FROM Contacts where assigned_to='$username'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Name</th>";
+    echo "<th>Email</th>";
+    echo "<th>Company</th>";
+    echo "<th>Type</th>";
+    echo "</tr>";
+
+    foreach($results as $row){
+        $assign=$row["id"];
+        $name = $row["title"]. ' ' . $row["firstname"]. ' '.$row["lastname"];
+        echo "<tr>";
+        echo "<td>".$name."</td>";
+        echo "<td>".$row["email"]."</td>";
+        echo "<td>".$row["company"]."</td>";
+        echo "<td>".$row["type"]."</td>";
+        echo "<td><span class='link' id='$assign'>view</span></td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
+}
 
 
 ?>
